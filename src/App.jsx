ffdb675ef29eccfa7d358a6cf88b8bdd7dde48ee
kaksv <kakoozavian@gmail.com>
2025-04-15 14:28:09 +0300

    widget worked on

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import Wormhole from '@wormhole-foundation/wormhole-connect'
+import WormholeConnect from '@wormhole-foundation/wormhole-connect'
 import './App.css'
 
 
+const theme = {
+  background: '#000',
+  text: '#fff',
+  border: '#fff',
+  buttonBackground: '#fff',
+  mode: 'dark',
+  primary: '#78c4b6',
+  font: 'Comic Sans, sans-serif', // Fixed the font declaration (removed semicolon)
+};
+
+const config = {
+  label: 'On Pay',
+}
+
 function App() {
-  
 
   return (
-    <>
-      <h1>The Connect Widget</h1>
-      <Wormhole />
-    </>
+    <div className="app-container">
+      {/* <h1>On Pay</h1>
+      <p>Seamless crosschain transfers.</p> */}
+      <div className="wormhole-container">
+        <WormholeConnect theme={theme} config={config} text="#fff" textSecondary="#f4f4f4" error="#ff0000" />
+      </div>
+    </div>
   )
 }
 
