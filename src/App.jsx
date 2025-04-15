7a8dbbcd040f639fbf8540b364087b49d1e09d30
kaksv <kakoozavian@gmail.com>
2025-04-15 09:36:28 +0300

    kickstarting cross-chain

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Wormhole from '@wormhole-foundation/wormhole-connect'
+import './App.css'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>The Connect Widget</h1>
+      <Wormhole />
+    </>
+  )
+}
+
+export default App
